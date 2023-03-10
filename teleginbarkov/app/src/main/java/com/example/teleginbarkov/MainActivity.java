1e3cefae4ff6d0214ed7cb4edf38d66126781609
QWERTygameover <91734242+QWERTygameover@users.noreply.github.com>
2023-03-10 13:43:24 +0700

    Add files via upload

---
+++ teleginbarkov/app/src/main/java/com/example/teleginbarkov/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.teleginbarkov;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    //commit 12222
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
